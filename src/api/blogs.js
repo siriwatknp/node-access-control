5cecb29a1b48647aeeb2d47f5526aebaf2ff2661
Siriwat Kunaporn <siriwatkunaporn@gmail.com>
2018-06-03 18:15:05 +0700

    use easy-session

--- src/api/blogs.js
+++ src/api/blogs.js
@@ -1,12 +1,55 @@
 import express from 'express'
+import session from 'express-session'
+import eSession from 'easy-session'
+import cookieParser from 'cookie-parser'
 import RBAC, { permissions } from '../lib/rbac'
 
 const rbac = new RBAC(permissions)
 
 const app = express()
+app.use(cookieParser())
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+}))
+app.use(eSession.main(session, {
+  rbac: (cb) => {
+    setImmediate(cb, null, {
+      guest: {
+        can: ['blog:read'],
+      },
+      writer: {
+        can: ['blog:create'],
+        inherits: ['guest']
+      }
+    })
+  }
+}))
+
+app.get('/login/:role', (req, res) => {
+  req.session.login(req.params.role)
+    .then(() => res.send('logged in'))
+})
+
+app.get('/logout', (req, res) => {
+  req.session.logout()
+    .then(() => res.send('logged out'))
+})
+
+app.get('/create', (req, res) => {
+  // if(!req.session.hasRole('writer')) {
+  //   res.sendStatus(403)
+  //   return
+  // }
+  // return res.send('Blog Edit')
+  req.session.can('blog:create')
+    .then(() => res.send('Blog created'))
+    .catch(() => res.sendStatus(403))
+})
 
 app.get('/', (req, res) => {
-  res.send(rbac.can('writer', 'edit', { user: { id: 1 }, post: { owner: 2 }}))
+  res.send('Current role is ' + req.session.getRole())
 })
 
 export default app
